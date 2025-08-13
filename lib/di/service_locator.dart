a81b4d796290307e9a287c1aa48f9d6c9b646d80
nezi404 <105327627+nezi404@users.noreply.github.com>
2025-08-12 22:42:01 -0400

    provider

--- lib/di/service_locator.dart
+++ lib/di/service_locator.dart
@@ -1,4 +1,6 @@
+import 'package:app4_receitas/data/repository/recipe_repository.dart';
 import 'package:app4_receitas/data/services/recipe_service.dart';
+import 'package:app4_receitas/ui/recipes/recipes_view_model.dart';
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -9,4 +11,8 @@ Future<void> setupDependencies() async {
 
   //Recipe service
   getIt.registerLazySingleton<RecipeService>(() => RecipeService());
+
+  getIt.registerLazySingleton<RecipeRepository>(()=>RecipeRepository());
+
+  getIt.registerLazySingleton<RecipesViewModel>(()=>RecipesViewModel());
 }
--- lib/ui/recipes/recipes_view.dart
+++ lib/ui/recipes/recipes_view.dart
@@ -1,4 +1,9 @@
+import 'package:app4_receitas/di/service_locator.dart';
+import 'package:app4_receitas/ui/recipes/recipes_view_model.dart';
+import 'package:app4_receitas/ui/widgets/recipe_card.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:go_router/go_router.dart';
 
 class RecipesView extends StatefulWidget {
   const RecipesView({super.key});
@@ -8,51 +13,54 @@ class RecipesView extends StatefulWidget {
 }
 
 class _RecipesViewState extends State<RecipesView> {
-  final viewModel = [];
+  final viewModel = getIt<RecipesViewModel>();
+
+
 
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      // viewModel.getRecipes();
+       viewModel.getRecipes();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // return Obx(() {
-      // if (viewModel.isLoading) {
-      //   return Center(
-      //     child: SizedBox(
-      //       height: 96,
-      //       width: 96,
-      //       child: CircularProgressIndicator(strokeWidth: 12),
-      //     ),
-      //   );
-      // }
+    return Obx(() {
+      if (viewModel.isLoading) {
+        return Center(
+          child: SizedBox(
+            height: 96,
+            width: 96,
+            child: CircularProgressIndicator(strokeWidth: 12),
+          ),
+        );
+      }
 
-      // if (viewModel.errorMessage != '') {
-      //   return Center(
-      //     child: Container(
-      //       padding: EdgeInsets.all(32),
-      //       child: Column(
-      //         spacing: 32,
-      //         children: [
-      //           Text(
-      //             'Erro: ${viewModel.errorMessage}',
-      //             style: TextStyle(fontSize: 24),
-      //           ),
-      //           ElevatedButton(
-      //             onPressed: () {
-      //               viewModel.getRecipes();
-      //             },
-      //             child: Text('TENTAR NOVAMENTE'),
-      //           ),
-      //         ],
-      //       ),
-      //     ),
-      //   );
-      // }
+      if (viewModel.errorMessage != '') {
+        return Center(
+          child: Container(
+            padding: EdgeInsets.all(32),
+            child: Column(
+              spacing: 32,
+              children: [
+                Text(
+                  'Erro: ${viewModel.errorMessage}',
+                  style: TextStyle(fontSize: 24),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    viewModel.getRecipes();
+                  },
+                  child: Text('TENTAR NOVAMENTE'),
+                ),
+              ],
+            ),
+          ),
+        );
+      }
+      
 
       return Container(
         padding: EdgeInsets.all(16),
@@ -60,106 +68,106 @@ class _RecipesViewState extends State<RecipesView> {
           children: [
             Expanded(
               child: 
-              // viewModel.recipes.isNotEmpty
-              //     ? Center(
-              //         child: Column(
-              //           children: [
-              //             Text(
-              //               '${viewModel.recipes.length} receitas(s)',
-              //               style: TextStyle(
-              //                 fontWeight: FontWeight.bold,
-              //                 fontSize: 16,
-              //               ),
-              //             ),
-              //             const SizedBox(height: 16),
-              //             Expanded(
-              //               child: ListView.builder(
-              //                 itemCount: viewModel.recipes.length,
-              //                 itemBuilder: (context, index) {
-              //                   final recipe = viewModel.recipes[index];
-              //                   final isFavorite = viewModel.favRecipes.any(
-              //                     (fav) => fav.id == recipe.id,
-              //                   );
-              //                   return Stack(
-              //                     children: [
-              //                       GestureDetector(
-              //                         onTap: () =>
-              //                             context.go('/recipe/${recipe.id}'),
-              //                         child: RecipeCard(recipe: recipe),
-              //                       ),
-              //                       Positioned(
-              //                         top: 16,
-              //                         right: 16,
-              //                         child: IconButton(
-              //                           icon: Icon(
-              //                             isFavorite
-              //                                 ? Icons.favorite
-              //                                 : Icons.favorite_border,
-              //                             size: 32,
-              //                             color: isFavorite ? Colors.red : null,
-              //                           ),
-              //                           onPressed: () {
-              //                             if (isFavorite) {
-              //                               viewModel.removeFromFavorites(
-              //                                 recipe,
-              //                               );
-              //                               if (context.mounted) {
-              //                                 ScaffoldMessenger.of(
-              //                                   context,
-              //                                 ).clearSnackBars();
-              //                                 ScaffoldMessenger.of(
-              //                                   context,
-              //                                 ).showSnackBar(
-              //                                   SnackBar(
-              //                                     content: Text(
-              //                                       '${recipe.name} desfavoritada!',
-              //                                     ),
-              //                                     duration: Duration(
-              //                                       seconds: 3,
-              //                                     ),
-              //                                     action: SnackBarAction(
-              //                                       label: 'DESFAZER',
-              //                                       onPressed: () {
-              //                                         viewModel.addToFavorites(
-              //                                           recipe,
-              //                                         );
-              //                                       },
-              //                                     ),
-              //                                   ),
-              //                                 );
-              //                               }
-              //                             } else {
-              //                               viewModel.addToFavorites(recipe);
-              //                               if (context.mounted) {
-              //                                 ScaffoldMessenger.of(
-              //                                   context,
-              //                                 ).clearSnackBars();
-              //                                 ScaffoldMessenger.of(
-              //                                   context,
-              //                                 ).showSnackBar(
-              //                                   SnackBar(
-              //                                     content: Text(
-              //                                       '${recipe.name} favoritada!',
-              //                                     ),
-              //                                     duration: const Duration(
-              //                                       seconds: 2,
-              //                                     ),
-              //                                   ),
-              //                                 );
-              //                               }
-              //                             }
-              //                           },
-              //                         ),
-              //                       ),
-              //                     ],
-              //                   );
-              //                 },
-              //               ),
-              //             ),
-              //           ],
-              //         ),
-              //       )
-              //     : 
+              viewModel.recipes.isNotEmpty
+                  ? Center(
+                      child: Column(
+                        children: [
+                          Text(
+                            '${viewModel.recipes.length} receitas(s)',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          Expanded(
+                            child: ListView.builder(
+                              itemCount: viewModel.recipes.length,
+                              itemBuilder: (context, index) {
+                                final recipe = viewModel.recipes[index];
+                                // final isFavorite = viewModel.favRecipes.any(
+                                //   (fav) => fav.id == recipe.id,
+                                // );
+                                return Stack(
+                                  children: [
+                                    GestureDetector(
+                                      onTap: () =>
+                                          context.go('/recipe/${recipe.id}'),
+                                      child: RecipeCard(recipe: recipe),
+                                    ),
+                                    // Positioned(
+                                    //   top: 16,
+                                    //   right: 16,
+                                    //   child: IconButton(
+                                    //     icon: Icon(
+                                    //       isFavorite
+                                    //           ? Icons.favorite
+                                    //           : Icons.favorite_border,
+                                    //       size: 32,
+                                    //       color: isFavorite ? Colors.red : null,
+                                    //     ),
+                                    //     onPressed: () {
+                                    //       if (isFavorite) {
+                                    //         viewModel.removeFromFavorites(
+                                    //           recipe,
+                                    //         );
+                                    //         if (context.mounted) {
+                                    //           ScaffoldMessenger.of(
+                                    //             context,
+                                    //           ).clearSnackBars();
+                                    //           ScaffoldMessenger.of(
+                                    //             context,
+                                    //           ).showSnackBar(
+                                    //             SnackBar(
+                                    //               content: Text(
+                                    //                 '${recipe.name} desfavoritada!',
+                                    //               ),
+                                    //               duration: Duration(
+                                    //                 seconds: 3,
+                                    //               ),
+                                    //               action: SnackBarAction(
+                                    //                 label: 'DESFAZER',
+                                    //                 onPressed: () {
+                                    //                   viewModel.addToFavorites(
+                                    //                     recipe,
+                                    //                   );
+                                    //                 },
+                                    //               ),
+                                    //             ),
+                                    //           );
+                                    //         }
+                                    //       } else {
+                                    //         viewModel.addToFavorites(recipe);
+                                    //         if (context.mounted) {
+                                    //           ScaffoldMessenger.of(
+                                    //             context,
+                                    //           ).clearSnackBars();
+                                    //           ScaffoldMessenger.of(
+                                    //             context,
+                                    //           ).showSnackBar(
+                                    //             SnackBar(
+                                    //               content: Text(
+                                    //                 '${recipe.name} favoritada!',
+                                    //               ),
+                                    //               duration: const Duration(
+                                    //                 seconds: 2,
+                                    //               ),
+                                    //             ),
+                                    //           );
+                                    //         }
+                                    //       }
+                                    //     },
+                                    //   ),
+                                    // ),
+                                  ],
+                                );
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  : 
                   Center(
                       child: Column(
                         children: [
@@ -184,6 +192,6 @@ class _RecipesViewState extends State<RecipesView> {
           ],
         ),
       );
-    // });
+    });
   }
 }
